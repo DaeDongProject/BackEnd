a927b603a3a45bad563d3550302df2be473e41a2
honeyWater <tkd2756@naver.com>
2023-05-31 22:01:29 +0900

    add Request about modifying wrong information

--- src/main/java/com/example/daedong/Dto/ChatRoom.java
+++ src/main/java/com/example/daedong/Dto/ChatRoom.java
@@ -3,12 +3,14 @@ package com.example.daedong.Dto;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @Document(collection = "ChatRoom")
 public class ChatRoom {
     private String _id;
     private String userId;
     private String chatTitle;
-    private Object[] contextUser;
+    private List<Object> contextUser;
     private boolean deleteYn;
 }
--- src/main/java/com/example/daedong/Main/Controller/MainController.java
+++ src/main/java/com/example/daedong/Main/Controller/MainController.java
@@ -2,6 +2,7 @@ package com.example.daedong.Main.Controller;
 
 import com.example.daedong.Dto.AnswerRequestDto;
 import com.example.daedong.Dto.ChatRoom;
+import com.example.daedong.Dto.ModifyRequestDto;
 import com.example.daedong.Main.Repository.UserRepository;
 import com.example.daedong.Main.Service.MainServiceImpl;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -31,20 +32,25 @@ public class MainController {
     public String AnswerRequest(@RequestBody AnswerRequestDto answerRequestDto) throws IOException {
         String answer = mainService.detectIntentTexts(answerRequestDto.getId(), PROJECT_ID, answerRequestDto.getQuestion(), SESSION_ID, "ko-KR");
 
-        if(answer.equals("fallback"))
+        if (answer.equals("fallback"))
             return mainService.processSearch(answerRequestDto.getId(), answerRequestDto.getQuestion());
         else
             return answer;
     }
 
     @GetMapping("/{userId}")
-    public ChatRoom NewestChat(@PathVariable String userId){
+    public ChatRoom NewestChat(@PathVariable String userId) {
         String chatId = mainService.findChatRoomObjectId(userId);
         return mainService.findById(chatId);
     }
 
     @GetMapping("/chatroom/{chatRoomId}")
-    public ChatRoom SelectedChat(@PathVariable String chatRoomId){
+    public ChatRoom SelectedChat(@PathVariable String chatRoomId) {
         return mainService.findById(chatRoomId);
+    }
+
+    @PostMapping("/chatroom/modify")
+    public String ModifyRequest(@RequestBody ModifyRequestDto modifyRequestDto){
+        return mainService.modifyInformation(modifyRequestDto);
     }
 }
