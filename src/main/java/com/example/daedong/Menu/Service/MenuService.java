af3453ecdbe93e297bcc46f5d5696e1908483528
ChangHee98 <dlackdgml2@naver.com>
2023-05-31 16:53:36 +0900

    MenuService 추가

--- src/main/java/com/example/daedong/Menu/Service/MenuService.java
+++ src/main/java/com/example/daedong/Menu/Service/MenuService.java
@@ -4,4 +4,7 @@ import org.springframework.stereotype.Service;
 
 public interface MenuService {
 
+    String createNewChat(String userId);
+
+
 }
--- src/main/java/com/example/daedong/Menu/Service/MenuServiceImpl.java
+++ src/main/java/com/example/daedong/Menu/Service/MenuServiceImpl.java
@@ -1,4 +1,14 @@
 package com.example.daedong.Menu.Service;
 
-public class MenuServiceImpl {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class MenuServiceImpl implements MenuService{
+    @Override
+    public String createNewChat(String userId) {
+        return null;
+    }
+
 }
